75912e7a0d6af44ec1b05d50889481a055ea7b9f
johnnyloreano <laureano.pedrojoao@gmail.com>
2020-06-01 17:05:44 -0300

    Refactor SIGNAL DIFFERENCE

--- src/main/java/com/pmendonca/Multiplicador.java
+++ src/main/java/com/pmendonca/Multiplicador.java
@@ -3,84 +3,85 @@ package com.pmendonca;
 public class Multiplicador {
     private Somador resultado;
 
-    public Multiplicador(Somador somador){
+    public Multiplicador(Somador somador) {
         this.resultado = somador;
     }
 
-    public Multiplicador set(Numero valor){
+    public Multiplicador set(Numero valor) {
         this.resultado = this.resultado.set(valor);
         return this;
     }
 
-    public Multiplicador vezes(Numero outro){
-        if(outro.valor()==0 || resultado.resultado().valor()==0){
-            //se um dos dois for 0, a resposta é zero porque 0*n=0
+    public Multiplicador vezes(Numero outro) {
+        if (outro.valor() == 0 || resultado.resultado().valor() == 0) {
+            // se um dos dois for 0, a resposta é zero porque 0*n=0
             resultado = resultado.set(new Numero(0));
             return this;
         }
-        //nas proximas 4 linhas descobre e armazena a informacao de valor positivo ou negativo dos numeros
-        boolean resultadoPos = true;
-        boolean outroPos = true;
-        if(resultado.resultado().valor()<0)resultadoPos=false;
-        if(outro.valor()<0)outroPos=false;
+        // nas proximas 4 linhas descobre e armazena a informacao de valor positivo ou
+        // negativo dos numeros
 
-        //transforma os dois valores em positivo para multiplicar com o while
-        resultado = resultado.set(resultado.resultado().abs());
-        outro = outro.abs();
+        boolean sinalDiferente = sinalDiferente(resultado, outro);
 
-        //multiplica
+        // multiplica
         Numero res = new Numero(resultado.resultado().valor());
-        while(outro.valor()>1){
+        while (outro.valor() > 1) {
             resultado.mais(res);
             outro.dec();
         }
-
-        //se os sinais forem diferentes, o resultado era pra ser negativo, entao transforma em negativo
-        if(resultadoPos!=outroPos){
+        // se os sinais forem diferentes, o resultado era pra ser negativo, entao
+        // transforma em negativo
+        if (sinalDiferente)
             resultado = resultado.set(new Numero(-resultado.resultado().valor()));
-        }
         return this;
     }
 
-    public Multiplicador dividido(Numero outro){
-        if(outro.valor()==0){
-            //divisao por 0 é indefinido, operacao invalida.
+    public Multiplicador dividido(Numero outro) {
+        if (outro.valor() == 0) {
+            // divisao por 0 é indefinido, operacao invalida.
             System.out.println("Divisao por 0, invalido!");
-            resultado=resultado.set(null);
+            resultado = resultado.set(null);
             return this;
         }
-        if(resultado.resultado().valor()==0){
-            //se o numero a ser divido for 0, o resultado é 0.
+        if (resultado.resultado().valor() == 0) {
+            // se o numero a ser divido for 0, o resultado é 0.
             resultado = resultado.set(new Numero(0));
             return this;
         }
-        //nas proximas 4 linhas descobre e armazena a informacao de valor positivo ou negativo dos numeros
-        boolean resultadoPos = true;
-        boolean outroPos = true;
-        if(resultado.resultado().valor()<0)resultadoPos=false;
-        if(outro.valor()<0)outroPos=false;
+        // nas proximas 4 linhas descobre e armazena a informacao de valor positivo ou
+        // negativo dos numeros
+        boolean sinalDiferente = sinalDiferente(resultado, outro);
 
-        //transforma os dois valores em positivo para multiplicar com o while
+        // transforma os dois valores em positivo para multiplicar com o while
         resultado = resultado.set(resultado.resultado().abs());
         outro = outro.abs();
 
-        //divide
-        //cria um count que conta quantas vezes podemos subtrair outro de resultado e o armazena como resultado.
+        // divide
+        // cria um count que conta quantas vezes podemos subtrair outro de resultado e o
+        // armazena como resultado.
         Numero count = new Numero(0);
-        while(resultado.resultado().valor() >= outro.valor()){
+        while (resultado.resultado().valor() >= outro.valor()) {
             count.inc();
             resultado.menos(outro);
         }
         resultado = resultado.set(new Numero(count.valor()));
 
-        //se os sinais forem diferentes, o resultado era pra ser negativo, entao transforma em negativo
-        if(resultadoPos!=outroPos){
+        // se os sinais forem diferentes, o resultado era pra ser negativo, entao
+        // transforma em negativo
+        if (sinalDiferente) {
             resultado = resultado.set(new Numero(-resultado.resultado().valor()));
         }
         return this;
     }
 
-    public Numero resultado(){
+    public Numero resultado() {
         return resultado.resultado();
     }
+
+    public boolean sinalDiferente(Somador soma, Numero numero) {
+        boolean resultadoPos = !(resultado.resultado().valor() < 0);
+        boolean outroPos = !(numero.valor() < 0);
+        return resultadoPos != outroPos;
+    }
+
 }
